a5ce44dd34c559a3a51e0ef3528269f01752e637
Aidan Johnson <johnsoac671@gmail.com>
2024-11-02 13:08:23 -0700

    added distance function

--- KNN.py
+++ KNN.py
@@ -1,4 +1,5 @@
 from processing import Vector, vectorize
+import math
 
 class KNN:
     
@@ -24,3 +25,22 @@ class KNN:
         prediction = round(sum(nearest_labels) / self.k)
         
         return prediction
+    
+    
+    def get_distance(self, vector, data):
+        
+        dot_product = sum(
+            [x[0] * x[1] for x in zip(vector, data)]
+            )
+        
+        vec_mag = self.get_magnitude(vector)
+        data_mag = self.get_magnitude(data)
+        
+        return dot_product / (vec_mag * data_mag)
+        
+
+    def get_magnitude(self, vector):
+        return math.sqrt(
+            sum(
+                [x*x for x in vector]
+                ))
