6928630f30ef5cad24b3281a1ce989c474b6e3c3
Aidan Johnson <johnsoac671@gmail.com>
2024-11-02 12:57:55 -0700

    temp values for calling in KNN, replace as needed

---
+++ processing.py
@@ -0,0 +1,11 @@
+class Vector:
+    
+    def __init__(self):
+        self.vector = 0
+        self.label = 0
+        self.id = 0
+        self.plaintext = "0"
+        
+
+def vectorize(data):
+    return data
