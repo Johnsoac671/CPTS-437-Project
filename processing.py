dc3ce1c1774a014055e16204a54f7d1ccc04f2e1
Aidan Johnson <johnsoac671@gmail.com>
2024-11-29 11:00:40 -0800

    added dot product

--- processing.py
+++ processing.py
@@ -7,3 +7,9 @@ class Vectorizer:
 
     def vectorize(self, data):
         return self.model.encode(data).tolist()
+
+
+def dot_product(a, b):
+    return sum(
+        [x[0] * x[1] for x in zip(a, b)]
+        )
