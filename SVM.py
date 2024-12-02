f56eb73f9936cbf50cf99cf2d6009040342d4965
Aidan Johnson <johnsoac671@gmail.com>
2024-12-02 14:31:26 -0800

    added precision and recall

--- SVM.py
+++ SVM.py
@@ -51,17 +51,31 @@ class SVM:
         self.models[target] = (w, b)
     
     
-    def test(self, test_set):
+    def test(self, test_set: pd.DataFrame):
+        """predicts each test tweet, and returns the accuracy"""
+        
+        labels = ["Positive", "Negative", "Neutral"]
+        precisions = {label:[0, 0] for label in labels} # True Positives : True and False Positives
+        recalls = {label:[0, 0] for label in labels} # True Positives : True Positives and False Negatives
+        
         correct = 0
         
         for tweet in test_set.itertuples():
             prediction, _ = self.predict(tweet.embedding)
             
+            precisions[prediction][1] += 1
+            recalls[tweet.sentiment][1] += 1
+            
             if prediction == tweet.sentiment:
                 correct += 1
+                precisions[prediction][0] += 1
+                recalls[tweet.sentiment][0] += 1
         
+        accuracy = correct / test_set.shape[0]
+        precision = {key : value[0] / value[1] for key, value in precisions.items()}
+        recall = {key : value[0] / value[1] for key, value in recalls.items()}
         
-        return correct / test_set.shape[0]
+        return accuracy, precision, recall
 
 
 
@@ -92,22 +106,44 @@ def training_testing_split(df, percentage, seed=None):
     return train_df, test_df
 
 
-labels = ["Positive", "Negative", "Neutral"]
+if __name__ == "__main__":
+    labels = ["Positive", "Negative", "Neutral"]
 
-classifier = SVM(labels, 1000, 0.001, 0.1)
-dataset = build_dataset("twitterData1000.csv")
+    classifier = SVM(labels, 200, 0.001, 0.1)
+    dataset = build_dataset("twitterData1000.csv")
 
-for _ in range(5):
-    training, testing = training_testing_split(dataset, 0.2)
-
-
-
-    for label in labels:
-        training_temp = replace_labels(training, label)
-        classifier.train(label, training_temp)
+    total_accuracy = 0
+    total_precision = {"Positive": 0, "Negative": 0, "Neutral": 0}
+    total_recall = {"Positive": 0, "Negative": 0, "Neutral": 0}
+    
+    for _ in range(5):
+        training, testing = training_testing_split(dataset, 0.2)
 
 
-    print(classifier.test(testing))
+
+        for label in labels:
+            training_temp = replace_labels(training, label)
+            classifier.train(label, training_temp)
+
+
+        accuracy, precision, recall = classifier.test(testing)
+        
+        total_accuracy += accuracy
+        
+        for label in precision:
+            total_precision[label] += precision[label]
+        for label in recall:
+            total_recall[label] += recall[label]
+    
+    avg_accuracy = total_accuracy / 5
+    avg_precision = {label: round(total_precision[label] / 5, 3) for label in total_precision}
+    avg_recall = {label: round(total_recall[label] / 5, 3) for label in total_recall}
+    
+    print(f"Accuracy: {round(avg_accuracy, 5)}, "
+          f"Precision: {avg_precision}, Recall: {avg_recall}")
+        
+    
+
 
     
 
